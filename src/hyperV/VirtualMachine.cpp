0fef78dbaa067f204eaf6108e6ef04b72b770f3b
Jim00000 <9379786+Jim00000@users.noreply.github.com>
2021-08-24 21:31:25 +0800

    Implement constructor & exception

--- src/hyperV/VirtualMachine.cpp
+++ src/hyperV/VirtualMachine.cpp
@@ -1,13 +1,38 @@
+#include "Common.hpp"
 #include "VirtualMachine.hpp"
 
 using namespace HyperV;
 
+namespace
+{
+    template <typename EXCEPTIONBASE, size_t SERIAL>
+    class VersatileException : public EXCEPTIONBASE
+    {
+    public:
+        VersatileException<EXCEPTIONBASE, SERIAL>() noexcept = default;
+        VersatileException<EXCEPTIONBASE, SERIAL>(const std::string &str) noexcept : EXCEPTIONBASE(str) {}
+        VersatileException<EXCEPTIONBASE, SERIAL>(std::string &&str) noexcept : EXCEPTIONBASE(std::move(str)) {}
+    };
+
+    using CreatePartitionFailedException = typename VersatileException<VirtualMachineException, 1>;
+}
+
 VirtualMachine::VirtualMachine()
 try : hPartition(nullptr)
 {
+    HRESULT Status = WHvCreatePartition(&hPartition);
+    if (Status != S_OK)
+        throw CreatePartitionFailedException(GetWin32LastError());
+}
+catch (const CreatePartitionFailedException &e)
+{
+    spdlog::critical(e.what());
+    throw VirtualMachineException("Creating Hyper-V Partition failed");
 }
 catch (...)
 {
+    spdlog::critical("Unidentified error got caught");
+    throw VirtualMachineException("Unknown error");
 }
 
 VirtualMachine::~VirtualMachine()
