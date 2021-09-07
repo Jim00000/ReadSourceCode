a58155e0d7f1cd69e01f97c1afbfeaedf6477986
Jim00000 <9379786+Jim00000@users.noreply.github.com>
2021-09-07 12:55:38 +0800

    Add Registers

---
+++ src/hyperV/Registers.cpp
@@ -0,0 +1,27 @@
+#include "Registers.hpp"
+
+using namespace HyperV;
+
+auto Registers::GeneralPurpose() const noexcept
+{
+    constexpr static WHV_REGISTER_NAME GeneralPurpose[] = {
+        WHvX64RegisterRax,
+        WHvX64RegisterRcx,
+        WHvX64RegisterRdx,
+        WHvX64RegisterRbx,
+        WHvX64RegisterRsp,
+        WHvX64RegisterRbp,
+        WHvX64RegisterRsi,
+        WHvX64RegisterRdi,
+        WHvX64RegisterR8,
+        WHvX64RegisterR9,
+        WHvX64RegisterR10,
+        WHvX64RegisterR11,
+        WHvX64RegisterR12,
+        WHvX64RegisterR13,
+        WHvX64RegisterR14,
+        WHvX64RegisterR15,
+        WHvX64RegisterRip,
+        WHvX64RegisterRflags};
+    return GeneralPurpose;
+}
---
+++ src/hyperV/Registers.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Common.hpp"
+
+namespace HyperV
+{
+    class Registers
+    {
+    public:
+        auto GeneralPurpose() const noexcept;
+    };
+
+}
